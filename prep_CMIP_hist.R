efac4af9ded95fe9c493b769e6a0ae945cc618f9
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2023-06-21 18:09:40 +1000

    Create prep_CMIP_hist.R

---
+++ prep_CMIP_hist.R
@@ -0,0 +1,36 @@
+library(raster)
+library(data.table)
+
+### Read in coordinates
+static.df <- fread('ft.static.csv')
+
+GCM='ACCESS1-0'
+scen='history'
+years='20002015'
+
+### Read in clim forcing
+tmax.ra <- readRDS(paste('../data/met/future/',GCM,'/',scen,'/',scen,'_',years,'_annual_tmax.rds',sep=''))
+prcp.ra <- readRDS(paste('../data/met/future/',GCM,'/',scen,'/',scen,'_',years,'_annual_pr.rds',sep=''))
+rh.ra <- readRDS(paste('../data/met/future/',GCM,'/',scen,'/',scen,'_',years,'_annual_rh.rds',sep=''))
+lai.ra <- readRDS(paste('../data/met/future/',GCM,'/',scen,'/',scen,'_',years,'_lai_jan_5km.rds',sep=''))
+seas.ra <- readRDS(paste('../data/met/future/',GCM,'/',scen,'/',scen,'_',years,'_pr_seasonality.rds',sep=''))
+
+### Stack variables
+met.stack = stack(list(tmax = tmax.ra,
+                       prcp = prcp.ra, 
+                       pr.seaonality = seas.ra, 
+                       rh = rh.ra,
+                       lai = lai.ra))
+
+### Convert to dataframe
+met.df <- as.data.frame(extract(met.stack,
+                        cbind(static.df$lon,static.df$lat)))
+
+### Combine static and climate predictors
+static.df$tmax.mean <- met.df$tmax
+static.df$map <- met.df$prcp
+static.df$rh.mean <- met.df$rh
+static.df$lai.opt.mean <- met.df$lai
+static.df$pr.seaonality <- met.df$pr.seaonality
+
+fwrite(static.df, paste('../cache/ft.',GCM,'.history.csv',sep=''))
