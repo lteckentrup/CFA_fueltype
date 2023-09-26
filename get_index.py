cce1df3b5f4b4755909252af948039995f1e4784
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2023-09-26 10:52:32 +1000

    Update get_index.py

--- get_index.py
+++ get_index.py
@@ -4,21 +4,11 @@ import numpy as np
 import rioxarray as rxr
 import pandas as pd
 
-### List of relevant fuel types
-fueltypes = [3001, 3002, 3003, 3005, 3006, 3007, 3008, 3009, 3010, 3011,
-             3012, 3013, 3014, 3015, 3021, 3022, 3023, 3024, 3025, 3026,
-             3027, 3028, 3029, 3043, 3047, 3048, 3049, 3050, 3051]
-
-'''
-Nearest neighbour remapping with gdalwarp from command line
-gdalwarp -r near -tr 90 90 VICSANSW161.tif VICSANSW161_90.tif
-'''
-
-### Read in regridded tif 
-da = xr.open_rasterio('../data/EVC_fuelType/evc/VICSANSW161_90.tif')
+### Read in regridded tif
+da = rxr.open_rasterio('../data/EVC_fuelType/evc/VICSANSW161_90.tif')
 
 ### Change projection for comparison with predictors
-da_wgs84 = da.rio.reproject('EPSG:4326')
+da_wgs84 = da[0].rio.reproject('EPSG:4326')
 
 ### Rename dimensions
 da_wgs84 = da_wgs84.rename({'x':'lon','y':'lat'})
@@ -26,15 +16,20 @@ da_wgs84 = da_wgs84.rename({'x':'lon','y':'lat'})
 ### Convert to dataframe
 df = da_wgs84.to_dataset(name='FT').to_dataframe()
 
-### Drop pixels outside Victoria
+### Drop fuel types outside Victoria
 df[df['FT'] >= 4000] = np.nan
 df[df['FT'] <= 2999] = np.nan
 
-### Drop irrelevant fuel types
-df = df[df['FT'].isin(fueltypes)].dropna()
+df.replace(3000, np.nan, inplace=True)
+df.replace(3047, np.nan, inplace=True)
+df.replace(3097, np.nan, inplace=True)
+df.replace(3098, np.nan, inplace=True)
+df.replace(3099, np.nan, inplace=True)
 
-### Drop nan, make dataframe pretty
-df.reset_index(inplace=True).drop(columns=['index','band','spatial_ref'],inplace=True)
+### Drop NaN
+df.dropna(inplace=True)
+df.reset_index(inplace=True)
+df.drop(columns=['band','spatial_ref'],inplace=True)
 
 ### Save dataframe
-df.to_csv('index.csv',index=False)
+df.to_csv('../cache/index.csv',index=False)
