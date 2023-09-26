42797129c0e51e9258f81eb464a69afcf8e33362
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2023-09-26 11:10:44 +1000

    Create get_hist_predictors.sh

---
+++ get_hist_predictors.sh
@@ -0,0 +1,11 @@
+### Generate historical predictor files for all GCMs
+
+make_CMIP_hist.R --GCM 'ACCESS1-0'
+make_CMIP_hist.R --GCM 'BNU-ESM'
+make_CMIP_hist.R --GCM 'CSIRO-Mk3-6-0'
+make_CMIP_hist.R --GCM 'GFDL-CM3'
+make_CMIP_hist.R --GCM 'GFDL-ESM2G'
+make_CMIP_hist.R --GCM 'GFDL-ESM2M'
+make_CMIP_hist.R --GCM 'INM-CM4'
+make_CMIP_hist.R --GCM 'IPSL-CM5A-LR'
+make_CMIP_hist.R --GCM 'MRI-CGCM3'
