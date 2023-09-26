86845435e7ee200660a06d2b8739edb08e796993
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2023-09-26 11:38:52 +1000

    Update prep_CMIP_fut.R

--- prep_CMIP_fut.R
+++ prep_CMIP_fut.R
@@ -1,15 +1,31 @@
 library(raster)
 library(data.table)
+library(argparse)
+
+### Set up arg parse
+parser <- ArgumentParser(description = 'Generate future projection predictor file for GCM of interest')
+parser$add_argument('--GCM', 
+                    dest='GCM', 
+                    help='Pass GCM name')
+parser$add_argument('--scen', 
+                    dest='scen', 
+                    help='Pass RCP scenario')
+parser$add_argument('--time', 
+                    dest='time', 
+                    help='Pass time span (mid or long)')
+parser$add_argument('--years', 
+                    dest='years', 
+                    help='Pass years (20452060 for mid;  20852100 for long)')
+
+### Parse arguments
+args <- parser$parse_args()
+GCM <- args$GCM
+scen <- args$scen
+time <- args$time
+years <- args$years
 
 ### Read in coordinates
 static.df <- fread('ft.static.csv')
-
-### Define GCM, scenarion, timeperiod
-GCM='ACCESS1-0'
-scen='rcp45'
-time='mid'
-years='20452060'
-
 
 ### Read in clim forcing
 tmax.ra <- readRDS(paste('../data/met/future/',GCM,'/',scen,'_',time,'/',scen,'_',years,'_annual_tmax.rds',sep=''))
@@ -18,7 +34,7 @@ rh.ra <- readRDS(paste('../data/met/future/',GCM,'/',scen,'_',time,'/',scen,'_',
 lai.ra <- readRDS(paste('../data/met/future/',GCM,'/',scen,'_',time,'/',scen,'_',years,'_lai_jan_5km.rds',sep=''))
 seas.ra <- readRDS(paste('../data/met/future/',GCM,'/',scen,'_',time,'/',scen,'_',years,'_pr_seasonality.rds',sep=''))
 
-### Stack variables
+### Stack met files
 met.stack = stack(list(tmax = tmax.ra,
                        prcp = prcp.ra, 
                        pr.seaonality = seas.ra, 
@@ -29,11 +45,12 @@ met.stack = stack(list(tmax = tmax.ra,
 met.df <- as.data.frame(extract(met.stack,
                         cbind(static.df$lon,static.df$lat)))
 
-### Combine static and climate features
+### Combine met and static predictors
 static.df$tmax.mean <- met.df$tmax
 static.df$map <- met.df$prcp
 static.df$rh.mean <- met.df$rh
 static.df$lai.opt.mean <- met.df$lai
 static.df$pr.seaonality <- met.df$pr.seaonality
 
+### Write CSV file
 fwrite(static.df, paste('../cache/ft.',GCM,'.',scen,'_',time,'.csv',sep=''))
