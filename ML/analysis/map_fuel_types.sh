a7d78344083c0c6a7ef987fc263a922484d673cb
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2024-02-10 09:55:41 +1100

    Update map_fuel_types.sh

--- ML/analysis/map_fuel_types.sh
+++ ML/analysis/map_fuel_types.sh
@@ -1,12 +1,14 @@
 ### For report, I only plotted the ensemble aggregate but script should also run
 ### on individual GCMs. E.g.:
-### python3.9 map_mode_projections.py --GCM ACCESS1-0 --scen rcp45 --timespan mid
+### python3.9 map_fuel_types.py --GCM ACCESS1-0 --scen rcp45 --timespan mid
+### or for the target dataset (arguments rcp45 and mid don't do anything for target)
+### python3.9 map_fuel_types.py --GCM Target --scen rcp45 --timespan mid
 
 for scen in rcp45 rcp85; do
     echo ${scen}
     for timespan in mid long; do
         echo ${timespan}
-        python3.9 map_mode_projections.py --GCM 'mode' --scen ${scen} \
-                                          --timespan ${timespan}
+        python3.9 map_fuel_types.py --GCM 'mode' --scen ${scen} \
+                                    --timespan ${timespan}
     done
 done
