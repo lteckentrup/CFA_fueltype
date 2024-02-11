cda3d96accfacce6093793d66a454b3cbdad88c8
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2024-02-12 10:24:56 +1100

    Create unseen_clim.sh

---
+++ ML/analysis/unseen_clim.sh
@@ -0,0 +1,10 @@
+for GCM in ACCESS1-0 BNU-ESM CSIRO-Mk3-6-0 GFDL-CM3 GFDL-ESM2G GFDL-ESM2M INM-CM4 IPSL-CM5A-LR MRI-CGCM3; do
+    echo ${GCM}
+    for scen in rcp45 rcp85; do
+        echo ${scen}
+        python3.9 unseen_clim.py --GCM 'ACCESS1-0' --scen 'rcp45' \
+                                 --timespan 'mid' --years '20452060'
+        python3.9 unseen_clim.py --GCM 'ACCESS1-0' --scen 'rcp45' \
+                                 --timespan 'long' --years '20852100'
+    done
+done
