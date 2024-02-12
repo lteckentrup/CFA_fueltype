bd693436023d64707c76423d80a469593face479
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2024-02-12 17:54:44 +1100

    Update map_eval_GPP_LAI_Fire.py

--- LPJ-GUESS/analysis/map_eval_GPP_LAI_Fire.py
+++ LPJ-GUESS/analysis/map_eval_GPP_LAI_Fire.py
@@ -8,26 +8,34 @@ import cartopy.feature as cfeature
 from  matplotlib.colors import ListedColormap, BoundaryNorm
 import matplotlib.patches as mpatches
 
+pathwayIN=('/data/hiestorage/WorkingData/MEDLYN_GROUP/PROJECTS/'
+           'dynamics_simulations/CFA/LPJ-GUESS/')
+
 ### Set up function to plot map
 def make_map(var,config,position,source):
-
+    global pathwayIN
+    
     ### Read in data: If data are based on LPJ-GUESS then
     if source == 'LPJ':
         if var == 'agpp':
-            ds = xr.open_dataset('../'+config+'/agpp_timmean.nc')
+            ds = xr.open_dataset(pathwayIN+'output/netCDF/eval_ERA5-Land/'+config+
+                                 '/agpp_timmean.nc')
             da = ds.Total[0]
             levels = np.arange(0,2.2,0.2)
         elif var == 'mlai':
-            ds = xr.open_dataset('../'+config+'/mlai_timmean.nc')
+            ds = xr.open_dataset(pathwayIN+'output/netCDF/eval_ERA5-Land/'+config+
+                                 '/mlai_timmean.nc')
             da = ds.mlai[0]
             levels = np.arange(0,5.5,0.5) 
         elif var == 'Fire':
-            ds = xr.open_dataset('../'+config+'/Fire_timmean.nc')
+            ds = xr.open_dataset(pathwayIN+'output/netCDF/eval_ERA5-Land/'+config+
+                                 '/Fire_timmean.nc')
             da = ds['Fire'][0]
             levels = [0,0.001,0.002,0.005,0.01,0.02,0.05,0.1,0.2]
             ncols=11
         elif var == 'BurntFr':
-            ds = xr.open_dataset('../'+config+'/annual_burned_area_timmean.nc')
+            ds = xr.open_dataset(pathwayIN+'output/netCDF/eval_ERA5-Land/'+config+
+                                 '/annual_burned_area_timmean.nc')
             da = ds['BurntFr'][0]
             levels = [0,0.01,0.02,0.05,0.1,0.2,0.5]
             ncols=9
@@ -35,20 +43,24 @@ def make_map(var,config,position,source):
     ### Read in data: If data are based on remote sensing or similar, then:
     else:
         if var == 'agpp':
-            ds = xr.open_dataset('AusEFlux_GPP_timmean.nc')
+            ds = xr.open_dataset(pathwayIN+'eval_datasets/processed/'
+                                 'AusEFlux_GPP_timmean.nc')
             levels = np.arange(0,2.2,0.2)
             da = ds[var][0]
         elif var == 'mlai':
-            ds = xr.open_dataset('MODIS_LAI_timmean.nc')
+            ds = xr.open_dataset(pathwayIN+'eval_datasets/processed/'
+                                 'MODIS_LAI_timmean.nc')
             levels = np.arange(0,5.5,0.5)
             da = ds[var][0]
         elif var == 'Fire':
-            ds = xr.open_dataset('CAMS-GFAS_fire_CO2_timmean.nc')
+            ds = xr.open_dataset(pathwayIN+'eval_datasets/processed/'
+                                 'CAMS-GFAS_fire_CO2_timmean.nc')
             da = ds['co2fire'][0]/12
             levels = [0,0.001,0.002,0.005,0.01,0.02,0.05,0.1,0.2]
             ncols=11
         elif var == 'BurntFr':
-            ds = xr.open_dataset('Fire_CCI_BF_timmean.nc')
+            ds = xr.open_dataset(pathwayIN+'eval_datasets/processed/'
+                                 'Fire_CCI_BF_timmean.nc')
             da = ds['burned_area'][0]/100
             levels = [0,0.01,0.02,0.05,0.1,0.2,0.5]
             ncols=9
@@ -154,63 +166,54 @@ axs=axs.flatten()
 
 ### Plot GPP comparison
 var='agpp'
-make_map(var,'',0,'True','') ### AusElux
-make_map(var,'Vic_default',1,'True','LPJ') ### LPJ global config
-make_map(var,'Vic_final',2,'True','LPJ') ### LPJ CFA config
+make_map(var,'',0,'') ### AusElux
+make_map(var,'LPJ_global',1,'LPJ') ### LPJ global config
+make_map(var,'LPJ_CFA',2,'LPJ') ### LPJ CFA config
 
 ### Plot LAI comparison: Take mlai (annual LAI is maximum LAI)
 var='mlai'
-make_map(var,'',3,'True','') ### MODIS
-make_map(var,'Vic_default',4,'True','LPJ') ### LPJ global config
-make_map(var,'Vic_final',5,'True','LPJ') ### LPJ CFA config
+make_map(var,'',3,'') ### MODIS
+make_map(var,'LPJ_global',4,'LPJ') ### LPJ global config
+make_map(var,'LPJ_CFA',5,'LPJ') ### LPJ CFA config
 
 ### Plot burned fraction comparison
 var='BurntFr'
-make_map(var,'',6,'True','') ### FireCCI
-make_map(var,'Vic_default',7,'True','LPJ') ### LPJ global config
-make_map(var,'Vic_final',8,'True','LPJ') ### LPJ CFA config
+make_map(var,'',6,'') ### FireCCI
+make_map(var,'LPJ_global',7,'LPJ') ### LPJ global config
+make_map(var,'LPJ_CFA',8,'LPJ') ### LPJ CFA config
 
 ### Plot fire CO2 emission comparison
 var='Fire'
-make_map(var,'',9,'True','') ### CAMS-GFAS
-make_map(var,'Vic_default',10,'True','LPJ') ### LPJ global config
-make_map(var,'Vic_final',11,'True','LPJ') ### LPJ CFA config
+make_map(var,'',9,'') ### CAMS-GFAS
+make_map(var,'LPJ_global',10,'LPJ') ### LPJ global config
+make_map(var,'LPJ_CFA',11,'LPJ') ### LPJ CFA config
+
+### Make lists for magic for loop to fix titles and labels
+titles=['GPP AusEFlux', 'GPP global config.', 'GPP VFA config.',
+        'LAI MODIS','LAI global config.','LAI VFA config.',
+        'BAF FireCCI','BAF global config.','BAF VFA config.',
+        'Fire CO$_2$ CAMS-GFAS','Fire CO$_2$ global config.',
+        'Fire CO$_2$ VFA config.']
+title_index=['a)','b)','c)','d)','e)','f)','g)','h)','i)']
+positions=[0,1,2,3,4,5,6,7,8]
 
 ### Set subplot titles
 fontsize=11
-axs[0].set_title('GPP AusEFlux',fontsize=fontsize)
-axs[1].set_title('GPP global config.',fontsize=fontsize)
-axs[2].set_title('GPP VFA config.',fontsize=fontsize)
-
-axs[3].set_title('LAI MODIS',fontsize=fontsize)
-axs[4].set_title('LAI global config.',fontsize=fontsize)
-axs[5].set_title('LAI VFA config.',fontsize=fontsize)
-
-axs[6].set_title('BAF FireCCI',fontsize=fontsize)
-axs[7].set_title('BAF global config.',fontsize=fontsize)
-axs[8].set_title('BAF VFA config.',fontsize=fontsize)
-
-axs[9].set_title('Fire CO$_2$ CAMS-GFAS',fontsize=fontsize)
-axs[10].set_title('Fire CO$_2$ global config.',fontsize=fontsize)
-axs[11].set_title('Fire CO$_2$ VFA config.',fontsize=fontsize)
-
-axs[0].set_title('a)', loc='left')
-axs[1].set_title('b)', loc='left')
-axs[2].set_title('c)', loc='left')
-axs[3].set_title('d)', loc='left')
-axs[4].set_title('e)', loc='left')
-axs[5].set_title('f)', loc='left')
-axs[6].set_title('g)', loc='left')
-axs[7].set_title('h)', loc='left')
-axs[8].set_title('i)', loc='left')
-axs[9].set_title('j)', loc='left')
-axs[10].set_title('k)', loc='left')
-axs[11].set_title('l)', loc='left')
+for t, ti, p in zip(titles, title_index, positions):
+    axs[p].set_title(t,fontsize=fontsize)
+    axs[p].set_title(ti, loc='left')
 
 ### Adjust plot layout
-plt.subplots_adjust(top=0.99, left=0.025, right=0.975, bottom=0.07,
+plt.subplots_adjust(top=0.99, left=0.05, right=0.975, bottom=0.05,
                     wspace=0.08, hspace=0.1)
 
-#plt.show()
-plt.savefig('figures/map_eval_GPP_LAI_Fire.png',dpi=400)
+### Adjust ticklabels on axes
+for p in (0,3,6,9):
+    ### Show ticklabels left
+    axs[p].yaxis.set_visible(True)
 
+for p in (9,10,11):
+    ### Show ticklabels bottom
+    axs[p].xaxis.set_visible(True)
+
+plt.savefig('figures/map_eval_GPP_LAI_Fire.png',dpi=400)
