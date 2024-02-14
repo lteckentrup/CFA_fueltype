55b100e85d4f3a2a016fdf19248afee706352592
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2024-02-14 12:33:28 +1100

    Update barplot_sens_cpool_fuel_group.py

--- LPJ-GUESS/analysis/barplot_sens_cpool_fuel_group.py
+++ LPJ-GUESS/analysis/barplot_sens_cpool_fuel_group.py
@@ -1,6 +1,6 @@
 import xarray as xr
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 import argparse
 
 '''
@@ -18,21 +18,25 @@ args = parser.parse_args()
 ### Assign variables
 sens=args.sens
 
-fig=plt.figure(figsize=(9,8))
+fig=plt.figure(figsize=(7,11))
 
-fig.subplots_adjust(hspace=0.17)
+fig.subplots_adjust(hspace=0.25)
 fig.subplots_adjust(wspace=0.25)
 fig.subplots_adjust(right=0.95)
-fig.subplots_adjust(left=0.1)
-fig.subplots_adjust(bottom=0.15)
-fig.subplots_adjust(top=0.9)
+fig.subplots_adjust(left=0.15)
+fig.subplots_adjust(bottom=0.13)
+fig.subplots_adjust(top=0.91)
 
-ax1=fig.add_subplot(3,2,1)
-ax2=fig.add_subplot(3,2,2)
-ax3=fig.add_subplot(3,2,3)
-ax4=fig.add_subplot(3,2,4)
-ax5=fig.add_subplot(3,2,5)
-ax6=fig.add_subplot(3,2,6)
+ax1=fig.add_subplot(5,2,1)
+ax2=fig.add_subplot(5,2,2)
+ax3=fig.add_subplot(5,2,3)
+ax4=fig.add_subplot(5,2,4)
+ax5=fig.add_subplot(5,2,5)
+ax6=fig.add_subplot(5,2,6)
+ax7=fig.add_subplot(5,2,7)
+ax8=fig.add_subplot(5,2,8)
+ax9=fig.add_subplot(5,2,9)
+ax10=fig.add_subplot(5,2,10)
 
 ### List of GCMs
 GCM_list = ['CNRM-CERFACS-CNRM-CM5', 'CSIRO-BOM-ACCESS1-0', 
@@ -42,7 +46,7 @@ GCM_list = ['CNRM-CERFACS-CNRM-CM5', 'CSIRO-BOM-ACCESS1-0',
 pathwayIN = ('/data/hiestorage/WorkingData/MEDLYN_GROUP/PROJECTS/'
              'dynamics_simulations/CFA/LPJ-GUESS/')
 
-def grab_data(GCM,scen,sens,FT_group,var,first_year,last_year):
+def grab_data(GCM,scen,sens,TFB_group,var,first_year,last_year):
     ### FPC is average over Vic, all carbon variables are sums
     if var == 'fpc':
         suffix = 'fldmean'
@@ -50,7 +54,7 @@ def grab_data(GCM,scen,sens,FT_group,var,first_year,last_year):
         suffix = 'fldsum'
 
     ### Read in all of Victora vs averages/sums aggregated over fuel groups
-    if FT_group == '':
+    if TFB_group == '':
         ds = xr.open_dataset(pathwayIN+'/output/netCDF/NHP/runs_'+
                              GCM+'_'+scen+'/'+var+'_1960-2099_'+suffix+'.nc')
         ds_sens = xr.open_dataset(pathwayIN+'/output/netCDF/NHP/runs_'+
@@ -58,9 +62,9 @@ def grab_data(GCM,scen,sens,FT_group,var,first_year,last_year):
                                   suffix+'.nc')
     else:
         ds = xr.open_dataset(pathwayIN+'/output/netCDF/NHP/runs_'+GCM+'_'+scen+
-                             '/'+FT_group+'/'+var+'_1960-2099_'+suffix+'.nc')
+                             '/'+TFB_group+'/'+var+'_1960-2099_'+suffix+'.nc')
         ds_sens = xr.open_dataset(pathwayIN+'/output/netCDF/NHP/runs_'+GCM+'_'+
-                                  scen+'_'+sens+'/'+FT_group+'/'+var+'_1960-2099_'+
+                                  scen+'_'+sens+'/'+TFB_group+'/'+var+'_1960-2099_'+
                                   suffix+'.nc')        
 
     if sens == 'nofire':   
@@ -81,7 +85,7 @@ def get_ens_stat(scen,sens,FT_group,var,first_year,last_year):
 
     return(df.sum().mean(),df.sum().std())
 
-def barplot_fuel_time(FT_group,sens,var1,var2,axis):
+def plot_barplot(FT_group,sens,var1,var2,axis):
     ### Set up for loop
     scenarios = ['RCP4.5', 'RCP8.5']
     periods = [('2045', '2059'), ('2085', '2099')]
@@ -140,40 +144,47 @@ def barplot_fuel_time(FT_group,sens,var1,var2,axis):
     axis.spines['top'].set_visible(False)
 
 ### Plot barplot
-barplot_fuel_time('',sens,'cmass','cmass_wood',ax1)
-barplot_fuel_time('Wet_forest',sens,'cmass','cmass_wood',ax3)
-barplot_fuel_time('Mallee',sens,'cmass','cmass_wood',ax5)
+plot_barplot('',sens,'cmass','cmass_wood',ax1)
+plot_barplot('East_Gippsland',sens,'cmass','cmass_wood',ax3)
+plot_barplot('North_East',sens,'cmass','cmass_wood',ax5)
+plot_barplot('Wimmera',sens,'cmass','cmass_wood',ax7)
+plot_barplot('Mallee',sens,'cmass','cmass_wood',ax9)
 
-barplot_fuel_time('',sens,'cmass_leaf','clitter',ax2)
-barplot_fuel_time('Wet_forest',sens,'cmass_leaf','clitter',ax4)
-barplot_fuel_time('Mallee',sens,'cmass_leaf','clitter',ax6)
+plot_barplot('',sens,'cmass_leaf','clitter',ax2)
+plot_barplot('East_Gippsland',sens,'cmass_leaf','clitter',ax4)
+plot_barplot('North_East',sens,'cmass_leaf','clitter',ax6)
+plot_barplot('Wimmera',sens,'cmass_leaf','clitter',ax8)
+plot_barplot('Mallee',sens,'cmass_leaf','clitter',ax10)
 
 ### Set xticklabels
-ax5.set_xticklabels(['Carbon stored\nin vegetation',
+ax9.set_xticklabels(['Carbon stored\nin vegetation',
                      'Carbon stored\nin wood'])
-ax6.set_xticklabels(['Carbon stored\nin leaves',
+ax10.set_xticklabels(['Carbon stored\nin leaves',
                      'Carbon stored\nin litter'])
 
 ### Set subplot titles
 axes = [ax1,ax2,ax3,ax4,ax5,ax6]
 title_labels = ['Victoria','Victoria',
-                'Wet forest','Wet forest',
+                'East Gippsland', 'East Gippsland',
+                'North East', 'North East',
+                'Wimmera', 'Wimmera',
                 'Mallee','Malle']
-title_indices = ['a)','b)','c)','d)','e)','f)']
+title_indices = ['a)','b)','c)','d)','e)',
+                 'f)','g)','h)','i)','j)']
 
 for ax, tl, ti in zip(axes,title_labels,title_indices):
     ax.set_title(tl)
     ax.set_title(ti,loc='left')
 
 ### Set y label
-for ax in (ax1,ax3,ax5):
-    ax.set_ylabel('$\Delta$ Carbon pool [PgC]')
+for ax in (ax1,ax3,ax5,ax7,ax9):
+    ax.set_ylabel('$\Delta$ Carbon pool\n[PgC]')
 
 ### Set legend
 ax1.legend(loc='upper center', 
-           bbox_to_anchor=(1.1, 1.5),
+           bbox_to_anchor=(1.1, 1.7),
            frameon=False,
            ncols=2)
 
 fig.align_ylabels()
-plt.savefig('figures/barplot_'+sens+'_cpool_fuel_group.pdf')
+plt.savefig('figures/barplot_'+sens+'_cpool_TFB.pdf')
